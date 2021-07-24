71f71c6892c3730f3f9256ac3684472623667ba2
arianne-toepfer <78578889+arianne-toepfer@users.noreply.github.com>
2021-07-23 18:22:17 -0700

    Update logic.js

--- static/js/logic.js
+++ static/js/logic.js
@@ -17,49 +17,52 @@ L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
 // Store API query variables
 var url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
 
-// Define a markerSize function that will give each earthquake marker a different size based on depth
-function markerSize(depth) {
-    return depth;
-}
-
-//create an array that holds the coordinates
-var coordArray = [];
-
 // Grab the data with d3
-d3.json(url, function(response) {
-    console.log(response);
+d3.json(url, function(data) {
+    console.log(data);
  
-    //create loop to pull the data from the geojson
-    for (var i = 0; i < response.length; i++){
+    //Define features
+    function CreateFeatures(magData) {
+        return magData.geometry.coordinates[2];
+    }
+    //create conditionals based on magnitude    
+    function colorCoord(magData) {    
         var color = "";
-        var coords = response[i].features.geometry;
-        if (coords.coordinates[2] > 90){
+        if (magData > 90){
             color = "red";
         }
-        else if (coords.coordinates[2] > 70){
+        else if (magData > 70){
             color="DarkOrange";
         }
-        else if (coords.coordinates[2] > 50){
+        else if (magData > 50){
             color="Orange";
         }
-        else if (coords.coordinates[2] > 30){
+        else if (magData > 30){
             color="Gold";
         }
-        else if (coords.coordinates[2] > 10){
+        else if (magData > 10){
             color="GreenYellow";
         }
         else {
             color="Chartreuse";
         }
-        // Add circles to map
-        L.circle([coords.coordinates[1], coords.coordinates[0]], {
-            fillOpacity: 0.75,
-            color: "white",
-            fillColor: color,
-            // Adjust radius
-            radius: markerSize(coords.coordinates[2])
-        }).bindPopup("<h3> Magnitude: " + coords.coordinates[2] + "</h3>").addTo(myMap);
     }
+    // Define a markerSize function that will give each earthquake marker a different size based on depth
+    function markerSize(magData) {
+    return magData;
+    }
+
+    // Add circles to map
+    
+
+    L.circle([coords.coordinates[1], coords.coordinates[0]], {
+        fillOpacity: 0.75,
+        color: "white",
+        fillColor: color,
+        // Adjust radius
+        radius: markerSize(coords.coordinates[2])
+    }).bindPopup("<h3> Magnitude: " + coords.coordinates[2] + "</h3>").addTo(myMap);
+    
 });
 // Set up the legend
 var legend = L.control({ position: "bottomright" });
